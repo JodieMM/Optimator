f66eca00d971579f2f66205d05d695a14bd7d91c
Jodie <jodiemmuller@gmail.com>
2019-03-15 10:35:47 +1000

    Created visuals for Settings form

--- Optimator/Animator/SettingsForm.Designer.cs
+++ Optimator/Animator/SettingsForm.Designer.cs
@@ -34,28 +34,30 @@
             this.BackColourBox = new System.Windows.Forms.PictureBox();
             this.BackColourLbl = new System.Windows.Forms.Label();
             this.ResetBtn = new System.Windows.Forms.Button();
+            this.SettingsLbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.BackColourBox)).BeginInit();
             this.SuspendLayout();
             // 
             // VersionLbl
             // 
             this.VersionLbl.AutoSize = true;
-            this.VersionLbl.Location = new System.Drawing.Point(143, 111);
+            this.VersionLbl.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VersionLbl.Location = new System.Drawing.Point(32, 83);
             this.VersionLbl.Name = "VersionLbl";
-            this.VersionLbl.Size = new System.Drawing.Size(121, 39);
+            this.VersionLbl.Size = new System.Drawing.Size(81, 25);
             this.VersionLbl.TabIndex = 0;
             this.VersionLbl.Text = "Version";
             // 
             // QuitBtn
             // 
-            this.QuitBtn.BackColor = System.Drawing.Color.Aquamarine;
+            this.QuitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.QuitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.QuitBtn.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuitBtn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.QuitBtn.ForeColor = System.Drawing.Color.Black;
-            this.QuitBtn.Location = new System.Drawing.Point(346, 503);
+            this.QuitBtn.Location = new System.Drawing.Point(469, 539);
             this.QuitBtn.Margin = new System.Windows.Forms.Padding(2);
             this.QuitBtn.Name = "QuitBtn";
-            this.QuitBtn.Size = new System.Drawing.Size(150, 80);
+            this.QuitBtn.Size = new System.Drawing.Size(170, 50);
             this.QuitBtn.TabIndex = 7;
             this.QuitBtn.Text = "Quit";
             this.QuitBtn.UseVisualStyleBackColor = false;
@@ -63,14 +65,14 @@
             // 
             // SaveBtn
             // 
-            this.SaveBtn.BackColor = System.Drawing.Color.Aquamarine;
+            this.SaveBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.SaveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.SaveBtn.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveBtn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SaveBtn.ForeColor = System.Drawing.Color.Black;
-            this.SaveBtn.Location = new System.Drawing.Point(346, 380);
+            this.SaveBtn.Location = new System.Drawing.Point(240, 539);
             this.SaveBtn.Margin = new System.Windows.Forms.Padding(2);
             this.SaveBtn.Name = "SaveBtn";
-            this.SaveBtn.Size = new System.Drawing.Size(150, 80);
+            this.SaveBtn.Size = new System.Drawing.Size(170, 50);
             this.SaveBtn.TabIndex = 8;
             this.SaveBtn.Text = "Save Changes";
             this.SaveBtn.UseVisualStyleBackColor = false;
@@ -80,9 +82,9 @@
             // 
             this.BackColourBox.BackColor = System.Drawing.Color.White;
             this.BackColourBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.BackColourBox.Location = new System.Drawing.Point(410, 208);
+            this.BackColourBox.Location = new System.Drawing.Point(249, 155);
             this.BackColourBox.Name = "BackColourBox";
-            this.BackColourBox.Size = new System.Drawing.Size(239, 30);
+            this.BackColourBox.Size = new System.Drawing.Size(49, 30);
             this.BackColourBox.TabIndex = 117;
             this.BackColourBox.TabStop = false;
             this.BackColourBox.Click += new System.EventHandler(this.BackColourBox_Click);
@@ -91,33 +93,44 @@
             // 
             this.BackColourLbl.AutoSize = true;
             this.BackColourLbl.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BackColourLbl.Location = new System.Drawing.Point(206, 199);
+            this.BackColourLbl.Location = new System.Drawing.Point(33, 159);
             this.BackColourLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.BackColourLbl.Name = "BackColourLbl";
-            this.BackColourLbl.Size = new System.Drawing.Size(184, 39);
+            this.BackColourLbl.Size = new System.Drawing.Size(203, 19);
             this.BackColourLbl.TabIndex = 116;
-            this.BackColourLbl.Text = "Back Colour";
+            this.BackColourLbl.Text = "Drawing Board Back Colour";
             // 
             // ResetBtn
             // 
-            this.ResetBtn.BackColor = System.Drawing.Color.Aquamarine;
+            this.ResetBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ResetBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ResetBtn.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetBtn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ResetBtn.ForeColor = System.Drawing.Color.Black;
-            this.ResetBtn.Location = new System.Drawing.Point(583, 308);
+            this.ResetBtn.Location = new System.Drawing.Point(11, 539);
             this.ResetBtn.Margin = new System.Windows.Forms.Padding(2);
             this.ResetBtn.Name = "ResetBtn";
-            this.ResetBtn.Size = new System.Drawing.Size(150, 80);
+            this.ResetBtn.Size = new System.Drawing.Size(170, 50);
             this.ResetBtn.TabIndex = 118;
             this.ResetBtn.Text = "Reset to Defaults";
             this.ResetBtn.UseVisualStyleBackColor = false;
             this.ResetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
             // 
+            // SettingsLbl
+            // 
+            this.SettingsLbl.AutoSize = true;
+            this.SettingsLbl.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SettingsLbl.Location = new System.Drawing.Point(30, 30);
+            this.SettingsLbl.Name = "SettingsLbl";
+            this.SettingsLbl.Size = new System.Drawing.Size(148, 39);
+            this.SettingsLbl.TabIndex = 119;
+            this.SettingsLbl.Text = "Settings";
+            // 
             // SettingsForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.MintCream;
-            this.ClientSize = new System.Drawing.Size(842, 676);
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(650, 600);
+            this.Controls.Add(this.SettingsLbl);
             this.Controls.Add(this.ResetBtn);
             this.Controls.Add(this.BackColourBox);
             this.Controls.Add(this.BackColourLbl);
@@ -143,5 +156,6 @@
         private System.Windows.Forms.PictureBox BackColourBox;
         private System.Windows.Forms.Label BackColourLbl;
         private System.Windows.Forms.Button ResetBtn;
+        private System.Windows.Forms.Label SettingsLbl;
     }
 }
